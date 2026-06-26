db8c2e87b8886b5e4678bc959ad1f3b14376616b
Sirapurapu Shyam <sirapurapushyam@gmail.com>
2026-06-26 10:46:03 +0530

    log

--- backend/src/services/google.service.js
+++ backend/src/services/google.service.js
@@ -1,11 +1,12 @@
 import { OAuth2Client } from "google-auth-library";
 import { env } from "../config/env.js";
 
-console.log("GOOGLE_CLIENT_ID:", env.GOOGLE_CLIENT_ID);
 
 const client = new OAuth2Client(env.GOOGLE_CLIENT_ID);
 
 export async function verifyGoogleToken(idToken) {
+console.log("GOOGLE_CLIENT_ID:", env.GOOGLE_CLIENT_ID);
+
 
   const ticket = await client.verifyIdToken({
     idToken,
